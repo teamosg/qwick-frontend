1264d3156d8437bfe9190f0234c31418e657e710
tarek8386 <tmahmud200@gmail.com>
2025-07-27 15:06:10 +0600

    fixes everything

--- src/pages/dashboard/DashboardHome.jsx
+++ src/pages/dashboard/DashboardHome.jsx
@@ -1,11 +1,10 @@
- import React, { useState } from 'react';
+ import NotFound from '@/shared/NotFound';
+import React, { useState } from 'react';
 
 const DashboardHome = () => {
   return (
     <div className="px-4 py-6 md:p-6 overflow-auto pt-2">
-      <h1 className='font-extrabold text-3xl md:text-6xl leading-[40px] w-full min-h-[500px] bg-red-300 flex justify-center items-center  '>
-        Content Goes here
-      </h1>
+     <NotFound/>
     </div>
   );
 };
