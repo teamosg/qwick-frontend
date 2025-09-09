4da13baf3529e2f68bc9f5ad4e54823df8ed199b
Tanjimul Islam <tanjimul.islam@hotmail.com>
2025-09-09 14:49:26 +0600

    discover page updated

--- src/components/dashboard/Profile/SecurityPrivacy.jsx
+++ src/components/dashboard/Profile/SecurityPrivacy.jsx
@@ -6,7 +6,7 @@ const SecurityPrivacy = () => {
       {/* Two-factor authentication section */}
       <div className="space-y-4">
         <div className="mb-8">
-          <h2 className="text-[18px] font-semibold text-[#090003] dark:text-white mb-4">
+          <h2 className="text-[18px] font-semibold text-[#090003] dark:text-white mb-1">
             Two-factor authentication
           </h2>
           <p className="text-[#717171] text-sm">
@@ -15,10 +15,12 @@ const SecurityPrivacy = () => {
         </div>
 
         {/* Text message card */}
-        <Card className="text-left text-[#717171] text-[16px] mb-8 py-10 w-full shadow rounded-[24px] ">
+        <Card className="text-left text-[#717171] text-[16px] mb-8 w-full shadow rounded-[24px] p-0">
           <CardContent className="p-6">
             <div className="space-y-2">
-              <h3 className="font-medium text-gray-900 dark:text-white">Text message</h3>
+              <h3 className="font-medium text-gray-900 dark:text-white">
+                Text message
+              </h3>
               <p className="text-sm text-gray-500">Receive a code via SMS</p>
             </div>
           </CardContent>
--- src/pages/dashboard/Discover.jsx
+++ src/pages/dashboard/Discover.jsx
@@ -38,6 +38,10 @@ const Discover = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.6 }}
     >
+      <h2 class="text-[24px] text-gray-900 dark:text-white font-semibold mb-4">
+        Campaigns
+      </h2>
+
       <motion.div
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center"
         variants={containerVariants}
