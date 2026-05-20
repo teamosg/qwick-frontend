5583c157d917d4359bb25e47db2ccdfde20b0e96
Arun Roy <arunroy0065@gmail.com>
2026-05-20 11:05:42 +0600

    refactor: format Navbar code and rename Dashboard link to Get Started

--- src/pages/landing/components/Navbar.jsx
+++ src/pages/landing/components/Navbar.jsx
@@ -30,11 +30,10 @@ const Navbar = () => {
     <motion.nav
       initial={{ y: -100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300   ${
-        isScrolled
-          ? "bg-transparent  border-slate-200/60 py-3  "
-          : "bg-transparent  border-transparent py-5"
-      }`}
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300   ${isScrolled
+        ? "bg-transparent  border-slate-200/60 py-3  "
+        : "bg-transparent  border-transparent py-5"
+        }`}
     >
       <div className="container mx-auto px-6 flex items-center justify-between">
         {/* Logo section */}
@@ -72,7 +71,7 @@ const Navbar = () => {
             to="/home"
             className="text-[14px] font-medium text-slate-600 px-7 py-2.5 rounded-full bg-gray-200 hover:bg-white hover:text-black hover:shadow-sm transition-all duration-300 font-inter"
           >
-            Dashboard
+            Get Started
           </Link>
         </div>
 
