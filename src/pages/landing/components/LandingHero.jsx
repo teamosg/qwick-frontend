729326af0535a968d628988c5f54bf6b32e9f7e2
Arun Roy <arunroy0065@gmail.com>
2026-05-20 11:10:20 +0600

    feat: replace Get Started button with React Router Link to navigation home route

--- src/pages/landing/components/LandingHero.jsx
+++ src/pages/landing/components/LandingHero.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { ArrowRight, Play } from 'lucide-react';
 import heroBg from '../../../assets/hero-bg-full.png';
+import { Link } from 'react-router';
 
 const LandingHero = () => {
   // Demo mode: Toggle between 'video' or 'image'
-  const backgroundMode = 'video'; 
+  const backgroundMode = 'video';
   // High-performance direct video link (Must be a direct .mp4/.webm file, NOT a YouTube link)
   const videoUrl = "https://cdn.pixabay.com/video/2024/05/26/213757_tiny.mp4";
 
@@ -25,9 +26,9 @@ const LandingHero = () => {
             className="w-full h-full object-cover"
           />
         ) : (
-          <img 
-            src={heroBg} 
-            alt="Background" 
+          <img
+            src={heroBg}
+            alt="Background"
             className="w-full h-full object-cover scale-[1.4]"
           />
         )}
@@ -60,10 +61,10 @@ const LandingHero = () => {
             transition={{ duration: 0.6, delay: 0.3 }}
             className="flex flex-col sm:flex-row  items-center gap-6"
           >
-            <button className="w-full sm:w-auto px-10 cursor-pointer py-5 bg-white text-black  rounded-2xl hover:bg-slate-100 transition-all flex items-center justify-center gap-3 group shadow-2xl shadow-white/10 uppercase tracking-widest text-sm font-inter">
+            <Link to="/home" className="w-full sm:w-auto px-10 cursor-pointer py-5 bg-white text-black  rounded-2xl hover:bg-slate-100 transition-all flex items-center justify-center gap-3 group shadow-2xl shadow-white/10 uppercase tracking-widest text-sm font-inter">
               Get Started
               <ArrowRight size={20} className="group-hover:translate-x-1 transition-transform" />
-            </button>
+            </Link>
           </motion.div>
         </div>
       </div>
